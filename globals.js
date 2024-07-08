f59a5b451b2b2fb09eaf451b0e02a06a229e8936
Sathvik <sathvikreddythumma@gmail.com>
2024-07-08 21:55:23 +0530

    latest1 commit

--- globals.js
+++ globals.js
@@ -5,3 +5,291 @@ button_construct.addEventListener("click",function(){
     //console.log("hi");
     
 });
+let dp1=document.getElementById("dp1");
+let dp2=document.getElementById("dp2");
+let dp3=document.getElementById("dp3");
+let dp4=document.getElementById("dp4");
+let dp5=document.getElementById("dp5");
+let dp6=document.getElementById("dp6");
+let dp7=document.getElementById("dp7");
+let dp8=document.getElementById("dp8");
+
+dp1.onclick=function(event){
+        document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+  //dp3.textContent="";
+    if (!event.target.matches('.chev-img')) {
+      //dp3.removeChild(myDropdown3);
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp2.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown2").classList.toggle("show");
+       // document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp3.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown3").classList.toggle("show");
+        //document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp4.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown4").classList.toggle("show");
+        //document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp5.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown5").classList.toggle("show");
+        //document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp6.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown6").classList.toggle("show");
+        //document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp7.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown7").classList.toggle("show");
+        //document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+        if(document.getElementById("myDropdown8").classList.toggle("show"))
+          document.getElementById("myDropdown8").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  dp8.onclick=function(){
+    //function myFunction() {
+        document.getElementById("myDropdown8").classList.toggle("show");
+        //document.getElementById("myDropdown1").classList.toggle("show");
+        if(document.getElementById("myDropdown2").classList.toggle("show"))
+          document.getElementById("myDropdown2").classList.toggle("show")
+        if(document.getElementById("myDropdown3").classList.toggle("show"))
+          document.getElementById("myDropdown3").classList.toggle("show")
+        if(document.getElementById("myDropdown4").classList.toggle("show"))
+          document.getElementById("myDropdown4").classList.toggle("show")
+        if(document.getElementById("myDropdown5").classList.toggle("show"))
+          document.getElementById("myDropdown5").classList.toggle("show")
+        if(document.getElementById("myDropdown6").classList.toggle("show"))
+          document.getElementById("myDropdown6").classList.toggle("show")
+        if(document.getElementById("myDropdown7").classList.toggle("show"))
+          document.getElementById("myDropdown7").classList.toggle("show")
+        if(document.getElementById("myDropdown1").classList.toggle("show"))
+          document.getElementById("myDropdown1").classList.toggle("show")
+      //}
+      
+      // Close the dropdown if the user clicks outside of it
+};
+window.onclick = function(event) {
+    if (!event.target.matches('.chev-img')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
